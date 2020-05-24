ec1b1e97e59c606f8a5b8dcf5c2900ad1f55ccee
Abdullah Zameek <arz268@nyu.edu>
2020-05-24 15:14:18 +0400

    stashing here

---
+++ build/precache-manifest.60dceae5d9c6eeb0fe94381e5f1a8608.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a5ef305fb1a71ee089de04412b7bfdf8",
+    "url": "/text_front/index.html"
+  },
+  {
+    "revision": "e371e409cd108f24f500",
+    "url": "/text_front/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "725cd5c5494c8c05020a",
+    "url": "/text_front/static/js/2.e30b3068.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/text_front/static/js/2.e30b3068.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e371e409cd108f24f500",
+    "url": "/text_front/static/js/main.7870f94a.chunk.js"
+  },
+  {
+    "revision": "5977e3334fca3929967f",
+    "url": "/text_front/static/js/runtime-main.8f0de43b.js"
+  }
+]);
